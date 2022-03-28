566c4fdde10c9d1dc1a7a2d66febd330483fc59a
venkatesh-15 <100559692+venkatesh-15@users.noreply.github.com>
2022-03-28 20:27:57 +0530

    Update javascrips.js

--- javascrips.js
+++ javascrips.js
@@ -1,4 +1,4 @@
-window.addEventListener('load',weather);
+
 function weather() {
   let long;
   let lat;
@@ -19,4 +19,21 @@ function weather() {
     let image = document.getElementById('setimage');
     image.src = imagelink;
   });
+}
+async function getlocationweather() {
+  let place = document.getElementById('searchlocation').value;
+  const api = `http://api.weatherapi.com/v1/current.json?key=c50a597f33804ab0b6a162647222603&q=${place}&aqi=no`;
+  let response = await fetch(api);
+  let parsed = await response.json();
+  console.log(parsed);
+  let tempf = document.getElementById('tempdegree');
+  tempf.innerHTML = parsed.current.temp_f;
+  let timezoneid = document.getElementById('tzid');
+  timezoneid.innerHTML = parsed.location.tz_id;
+  let condition = document.getElementById('tempcondition');
+  condition.innerHTML = parsed.current.condition.text;
+  let imagelink = parsed.current.condition.icon;
+  let image = document.getElementById('setimage');
+  image.src = imagelink;
+
 }
